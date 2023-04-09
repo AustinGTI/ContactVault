3143b6b88ee5d57b76d726279554732f16df0e50
AustinGTI <gathiiaustin@gmail.com>
2023-04-09 13:01:45 +0300

    basic wireframe for the contacts list (dashboard) page

--- src/contacts_pages/ContactsListPage.tsx
+++ src/contacts_pages/ContactsListPage.tsx
@@ -1,27 +1,24 @@
 import React, {useCallback, useEffect} from "react";
 import {ContactServiceClient} from "../protos/ContactsServiceClientPb";
-import {Empty} from "../protos/contacts_pb";
+import {Contact, ContactList, Empty} from "../protos/contacts_pb";
 import {Navigate, useNavigate} from "react-router-dom";
-
-type Contact = { id: string, name: string, email: string, phone: string };
-type ContactsList = Contact[];
-
+import {getContacts} from "../globals/client_functions";
 
 
 // a component to display a single contact pane
-function ContactPane({contact} : {contact : Contact}) : React.ReactElement {
+function ContactPane({contact}: { contact: Contact }): React.ReactElement {
     return (
         <div className="contact-pane">
-            <h2>{contact.name}</h2>
-            <p>{contact.email}</p>
-            <p>{contact.phone}</p>
+            <h2>{contact.getName()}</h2>
+            <p>{contact.getEmail()}</p>
+            <p>{contact.getPhone()}</p>
         </div>
     );
 }
 
 
-export default function ContactsListPage() : React.ReactElement {
-    const [contacts, setContacts] = React.useState<ContactsList>([]);
+export default function ContactsListPage(): React.ReactElement {
+    const [contacts, setContacts] = React.useState<Contact[]>([]);
     const navigate = useNavigate();
 
     // function to handle adding a new contact
@@ -32,27 +29,16 @@ export default function ContactsListPage() : React.ReactElement {
 
     // retrieve the contacts from the server on mount
     useEffect(() => {
-        const client = new ContactServiceClient("http://localhost:8080");
-        const request = new Empty();
-        const jwt_token = localStorage.getItem('jwt_token');
-        const metadata = jwt_token ? {authorization: jwt_token} : null;
-        client.getContacts(request, metadata, (err, res) => {
-            if (err) {
-                console.error(err);
-            } else {
-                const contacts = res.getContactsList().map((contact) => {
-                    return {
-                        id: contact.getId(),
-                        name: contact.getName(),
-                        email: contact.getEmail(),
-                        phone: contact.getPhone(),
-                    };
-                });
-                setContacts(contacts);
-                console.log('contacts set', contacts);
-            }
+        // if the jwt token is not present, it is treated as an invalid token
+        getContacts(`${localStorage.getItem('jwt_token')}`).then((contact_list) => {
+            setContacts(contact_list.getContactsList());
+        }).catch((err) => {
+            // if there is an error, redirect to the login page
+            navigate('/login');
+            console.log(err);
         });
-    }, []);
+    }, [navigate]);
+
     return (
         <div className="contacts">
             <h1>Contacts</h1>
@@ -60,7 +46,7 @@ export default function ContactsListPage() : React.ReactElement {
             <button className="add-contact-button" onClick={addContactHandler}>Add Contact</button>
             <div className="contacts-list">
                 {contacts.length ? contacts.map((contact) => {
-                    return <ContactPane contact={contact} key={contact.id}/>
+                    return <ContactPane contact={contact} key={contact.getId()}/>
                 }) : <p>No contacts yet</p>}
             </div>
         </div>
